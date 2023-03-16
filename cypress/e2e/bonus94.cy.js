98e6faf7fed850b9d1e16ac619167a9d6756213e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-16 13:19:58 -0400

    add bonus 94

---
+++ cypress/e2e/bonus94.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+it('hides API calls we are not interested in', () => {
+  // get the Cypress version major and minor
+  // https://on.cypress.io/version
+  //
+  // log the parsed version major and minor numbers
+  // to the Cypress Command Log
+  //
+  // if the version is >= 12.8.0
+  //  figure out the best strategy for hiding the calls
+  //  we are not interested in (like "POST /track")
+  //  1: hide only the "POST /track" calls
+  //  cy.intercept('POST', '/track', { log: false })
+  //  2: hide all fetch API calls
+  // https://on.cypress.io/intercept
+  cy.visit('/calculator.html')
+  cy.get('#num1').type(20)
+  cy.get('#num2').type(6)
+  cy.intercept('POST', '/calculate').as('calculate')
+  cy.get('#add').click()
+  cy.contains('#answer', 26)
+})

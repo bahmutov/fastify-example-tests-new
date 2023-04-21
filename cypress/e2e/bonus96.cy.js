b92d8119a577ab110bcd5b4e473aaf55e21fd9ec
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-21 09:58:09 -0400

    add bonus96

---
+++ cypress/e2e/bonus96.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+it('loads two CSS resources', () => {
+  // spy on all stylesheet resources
+  // and give the intercept alias "css"
+  // https://on.cypress.io/intercept
+  // https://on.cypress.io/as
+  //
+  // visit the calculator page "/calculator.html"
+  // https://on.cypress.io/visit
+  //
+  // confirm that there are 2 css intercepts
+  //
+  // confirm there were two resources loaded by the page
+  // window.performance.getEntriesByType
+  // https://on.cypress.io/window
+  // https://on.cypress.io/its
+  // https://on.cypress.io/invoke
+  //
+  // filter these entries to only the resources
+  // with names ending with ".css"
+  // there should be two of them
+  //
+  // convert entries into the list of their names
+  // and print the list of names
+  // to the Cypress Command Log
+})

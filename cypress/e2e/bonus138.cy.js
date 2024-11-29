20aeef635679ed1c52355c6f847060697bfb250a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-11-29 17:22:19 -0500

    bonus 138

--- cypress/e2e/bonus138.cy.js
+++ cypress/e2e/bonus138.cy.js
@@ -1,8 +1,21 @@
 /// <reference types="cypress" />
 
-it('loads the data on the slow page (test 1)', () => {
+let response
+
+beforeEach(() => {
+  // speed up the "GET /delay/*" calls
+  // the application makes in each test:
+  //
+  // spy on the very first "GET /delay/*" call
+  // and store its response in the "response" variable
+  // https://on.cypress.io/intercept
+  // for other calls we will use the stored response
+  // by setting it as a stub response
   cy.visit('/slow-page.html')
   cy.get('#loading').should('have.text', 'loading...')
+})
+
+it('loads the data on the slow page (test 1)', () => {
   cy.get('#loading', { timeout: 5500 }).should(
     'have.text',
     'data loaded',
@@ -10,8 +23,6 @@ it('loads the data on the slow page (test 1)', () => {
 })
 
 it('loads the data on the slow page (test 2)', () => {
-  cy.visit('/slow-page.html')
-  cy.get('#loading').should('have.text', 'loading...')
   cy.get('#loading', { timeout: 5500 }).should(
     'have.text',
     'data loaded',
@@ -19,8 +30,6 @@ it('loads the data on the slow page (test 2)', () => {
 })
 
 it('loads the data on the slow page (test 3)', () => {
-  cy.visit('/slow-page.html')
-  cy.get('#loading').should('have.text', 'loading...')
   cy.get('#loading', { timeout: 5500 }).should(
     'have.text',
     'data loaded',
